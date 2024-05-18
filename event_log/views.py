d030da253b8118fc0db421b6914f627e862946cf
cmaraya <catalina.araya.i@usach.cl>
2024-05-17 23:13:17 -0400

    Add chart info

--- event_log/views.py
+++ event_log/views.py
@@ -1,26 +1,31 @@
 # Create your views here.
 from django.http import JsonResponse
 from event_log.models import EventLog
-from datetime import datetime
-from django.utils.timezone import make_aware
-
+from datetime import datetime, timedelta
 
 def all_event_logs(request):
   item_to_show = request.GET.get('item_to_show')
   event_logs = EventLog.objects.all().values()[:int(item_to_show)]
-  return JsonResponse(list(event_logs), safe=False)
+  chart_data =get_chart_data(event_logs)
+  # Devolver datos
+  response_data = {
+    'event_logs': list(event_logs),
+    'chart_labels':chart_data[0],
+    'chart_total':chart_data[1]
+   }
+  return JsonResponse(response_data, safe=False)
 
 def get_unique_parameters(parameter,tiems):
-    # Obtener todos los logs de eventos
-    event_logs = EventLog.objects.values(parameter)
-    # Filtrar para incluir solo eventos del 03-04-2023
-    start_date = datetime.strptime('2023-04-03', '%Y-%m-%d')
-    end_date = start_date.replace(hour=23, minute=59, second=59)
-    event_logs = event_logs.filter(time__range=(start_date, end_date))
-
-    usernames = event_logs[:int(tiems)]
-    usernames_list = list(usernames)
-    return JsonResponse(usernames_list, safe=False)
+  # Obtener todos los logs de eventos
+  event_logs = EventLog.objects.values(parameter)
+  # Filtrar para incluir solo eventos del 03-04-2023
+  start_date = datetime.strptime('2023-04-03', '%Y-%m-%d')
+  end_date = start_date.replace(hour=23, minute=59, second=59)
+  event_logs = event_logs.filter(time__range=(start_date, end_date))
+  #Lista de parametros unics que se encuentran en ciertos eventos
+  usernames = event_logs[:int(tiems)]
+  usernames_list = list(usernames)
+  return JsonResponse(usernames_list, safe=False)
 
 def get_unique_usernames(request):
   item_to_show = request.GET.get('item_to_show')
@@ -33,6 +38,43 @@ def get_unique_event_types(request):
 def get_unique_event_sources(request):
   item_to_show = request.GET.get('item_to_show')
   return get_unique_parameters('event_source',item_to_show)
+
+def make_string_date(date):
+  new_date= date - timedelta(hours=4)
+  return new_date.strftime("%H:%M:%S")
+
+def make_time_label(date, interval):
+  return str(make_string_date(date) +" hasta " + make_string_date(date + interval))
+
+def get_chart_data(event_logs):
+  # Iniciar tiempo base
+  intital_time =  event_logs[0]["time"]
+  # Definir el intervalo de tiempo (en minutos, en este caso 5 minutos)
+  interval = timedelta(minutes=5)
+  # Inicializo la lista de etiquetas
+  labels = []
+  # Agrego el primer intervalo de tiempo de las etiquetas
+  labels.append(make_time_label(intital_time, interval))
+  # Inicializo la lista de total de eventos encontrado en el intervalo
+  total = []
+  # Creo el primervalor de intervalo final
+  current_end_interval = intital_time + interval
+  # Inicializo la cuenta de eventos
+  count=0
+  # Recorrer los eventos
+  for event in event_logs:
+    count = count +1
+    # Si llega al intervao superior, se crea un nuevo intervalo y se guarda el total de eventos encontrados
+    if(event["time"]>=current_end_interval):
+      labels.append(make_time_label(current_end_interval, interval))
+      current_end_interval= current_end_interval + interval
+      total.append(count)
+      count= 0
+  # Agrego la cuenta final pertenciente al ultimo intervalo
+  total.append(count)
+  chart_data=[labels, total]
+
+  return chart_data
 
 def get_filter_event_logs(request):
     # Obtener los parámetros de la solicitud
@@ -78,11 +120,16 @@ def get_filter_event_logs(request):
 
     # Obtener la cantidad pedida de eventos
     event_logs = event_logs[:int(item_to_show)]
+
+    chart_data =get_chart_data(event_logs)
+    # Devolver datos
     response_data = {
         'event_logs': list(event_logs),
         'event_types': list(unique_event_types),
         'event_sources': list(unique_event_sources),
         'usernames': list(unique_usernames),
+        'chart_labels':chart_data[0],
+        'chart_total':chart_data[1]
     }
     # Devolver json con eventos y nuevos parametros de los select correcpondientes
     return JsonResponse(response_data, safe=False)
